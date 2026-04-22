d809e8926bdd3f5c676c56ab92ce363e9631fc09
Francisco Junior <juniorvasconcelos06@gmail.com>
2026-04-22 15:42:07 -0300

    "refactory: método refatorado para retornar tipo String, não mais int."

--- src/main/java/br/com/biblioteca/factory/UsuarioFactory.java
+++ src/main/java/br/com/biblioteca/factory/UsuarioFactory.java
@@ -5,15 +5,17 @@ import br.com.biblioteca.model.Aluno;
 import br.com.biblioteca.model.Professor;
 
 public class UsuarioFactory {
-    public static Usuario criarUsuario(int tipo, String nome){
-        switch (tipo){
-            case 1:
+    public static Usuario criarUsuario(String tipo, String nome) {
+        if (tipo == null || tipo.isBlank()) {
+            throw new RuntimeException("Tipo não pode ser vazio.");
+        }
+        switch (tipo.trim()) {
+            case "Aluno":
                 return new Aluno(nome);
-            case 2:
+            case "Professor":
                 return new Professor(nome);
             default:
-                System.out.println("Tipo de utilizador desconhecido.");
-                return null;
+                throw new RuntimeException("Tipo desconhecido: " + tipo);
         }
     }
 }
